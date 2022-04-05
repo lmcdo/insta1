949ec94488d648edbed924a829e1fbd1eaa9c796
Lawrence McDonell <lawrence.mcdonell@gmail.com>
2022-04-05 12:46:42 +1000

    insta logo and search input OK

---
+++ components/Header.js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+import {
+	SearchIcon,
+	PlusCircleIcon,
+	UserGroupIcon,
+	HeartIcon,
+	PaperAirplaneIcon,
+	MenuIcon,
+} from "@heroicons/react/solid";
+
+
+function Header() {
+	return (
+		<div>
+			<div className=""></div>
+			<div className="flex justify-between max-w-6xl">
+				{/* 				/* Left */}
+				<div className="relative hidden lg:inline-grid h-24 w-24">
+					<Image
+						src="https://links.papareact.com/ocw"
+						layout="fill"
+						objectFit="contain"
+					/>
+				</div>
+				<div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+					<Image
+						src="https://links.papareact.com/jjm"
+						layout="fill"
+						objectFit="contain"
+					/>
+				</div>
+				{/* 	/* Middle Search input field */}
+				<div className="max-w-xs">
+					<div className="relative mt-1 p-3 rounded-md">
+						<div className="absolute inset-y-0 pl-3 flex items-center
+						 pointer-events-none">	
+							<SearchIcon className="h-5 w-5 text-gray-500" />
+						</div>
+						<input
+							className="bg-gray-50 block w-full pl-10 
+							sm:text-sm border-gray-300 focus:ring-black focus:border-black 
+							rounded-md"
+							type="text"
+							placeholder="Search"
+						/>
+					</div>
+				</div>
+				{/* Right */}
+				{/* Bottom */}
+			</div>
+		</div>
+	);
+}
+
+export default Header;
