795df9ef72bc2435ece95a1b2739a143273fce6c
Lawrence McDonell <lawrence.mcdonell@gmail.com>
2022-04-10 09:55:57 +1000

    retrieve post data from firebase storage and display OK

--- components/Posts.js
+++ components/Posts.js
@@ -1,44 +1,36 @@
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
-const posts = [
-	{
-		id: "123",
-		username: "llllala",
-		userImg: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		img: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		caption: "subscribe and destroy the like button",
-	},
-	{
-		id: "123",
-		username: "llllala",
-		userImg: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		img: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		caption: "subscribe and destroy the like button",
-	},
-	{
-		id: "123",
-		username: "llllala",
-		userImg: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		img: "https://i.ibb.co/Ptgx60W/P1010859.jpg",
-		caption: "subscribe and destroy the like button",
-	},
-];
-
 function Posts() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(
+		() => 
+			onSnapshot(
+				query(collection(db, "posts"), orderBy("timestamp", "desc")),
+				(snapshot) => {
+					setPosts(snapshot.docs);
+				}
+			),		
+		[db]
+	);
+
+	console.log(posts);
 	return (
-		<div>
+		 <div>
 			{posts.map((post) => (
 				<Post
 					key={post.id}
 					id={post.id}
-					username={post.username}
-					userImg={post.userImg}
-					img={post.img}
-					caption={post.caption}
+					username={post.data().username}
+					userImg={post.data().profileImg}
+					img={post.data().image}
+					caption={post.data().caption}
 				/>
 			))}
-		</div>
+		</div> 
 	);
 }
 
