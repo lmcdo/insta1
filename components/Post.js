1167cf96b0b5245ee322088b728b21b02a827f65
Lawrence McDonell <lawrence.mcdonell@gmail.com>
2022-04-10 18:22:47 +1000

    Like and Unlike OK

--- components/Post.js
+++ components/Post.js
@@ -122,6 +122,9 @@ function Post({ id, username, userImg, img, caption }) {
 			
 
           <p className='p-5 truncate'>
+              {likes.length > 0 && (
+                  <p className="font-bold mb-1">{likes.length} likes</p>
+              )}
           <span className="font-bold mr-l">{username} </span>
               {caption}
           </p>
